01177415901bfdb1134212ab181b7c587acfa4ee
farad-alam <farad.alam1997@gmail.com>
2024-07-08 10:56:06 +0600

    User account details display, and updated funtionality added

--- src/flaskblog/users/forms.py
+++ src/flaskblog/users/forms.py
@@ -3,6 +3,7 @@ from flask_wtf.file import FileAllowed
 from wtforms import StringField, SubmitField, EmailField, PasswordField, FileField, BooleanField
 from wtforms.validators import DataRequired, EqualTo, Length, Email, ValidationError
 from .models import User
+from flask_login import current_user
 
 class UserRegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -27,4 +28,23 @@ class UserLoginForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+
+
+class User_Account_Update_Form(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    image = FileField('Profile Image (Optional)', validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user and user.username != current_user.username:
+            raise ValidationError(f"This username '{username.data}' is already taken, please choose different one!")
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(username=email.data).first()
+        if user and user.email != current_user.email:
+            raise ValidationError(f"This email '{email.data}' is already taken, please choose different one!")
 
--- src/flaskblog/users/routes.py
+++ src/flaskblog/users/routes.py
@@ -1,9 +1,12 @@
 from .models import User
-from flask import Blueprint, render_template, flash, redirect, url_for, request
+from flask import Blueprint, render_template, flash, redirect, url_for, request, current_app
 from flaskblog import db
-from .forms import UserRegistrationForm, UserLoginForm
+from .forms import UserRegistrationForm, UserLoginForm, User_Account_Update_Form
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import current_user, login_user, logout_user
+from werkzeug.utils import secure_filename
+from flask_login import current_user, login_user, logout_user, login_required
+from PIL import Image
+import os, secrets
 
 
 
@@ -59,3 +62,65 @@ def user_login():
 def user_logout():
     logout_user()
     return redirect(url_for('user_bp.user_login'))
+
+@user_bp.route('/user/account')
+@login_required
+def user_account():
+    return render_template('account.html', title='Account', current_user=current_user)
+
+
+
+
+
+
+def save_profile_image(image_data):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(image_data.filename)
+    filename = secure_filename(f"{random_hex}{f_ext}")
+    filepath = os.path.join(current_app.root_path, 'static/profile_pics')
+
+    # Ensure the directory exists
+    if not os.path.exists(filepath):
+        os.makedirs(filepath)
+
+    full_filepath = os.path.join(filepath, filename)
+
+    # Resize and save the image
+    output_size = (100, 100)
+    image = Image.open(image_data)
+    image.thumbnail(output_size)
+    image.save(full_filepath)
+
+    return filename
+
+
+@user_bp.route('/user/edit/<int:user_id>', methods=['GET', 'POST'])
+@login_required
+def user_account_edit(user_id):
+    user = User.query.get_or_404(user_id)
+    if user != current_user:
+        flash('You can only edit your own account!', 'danger')
+        return redirect(url_for('main_bp.home'))
+    
+    form = User_Account_Update_Form()
+
+    if form.validate_on_submit():
+        if form.username.data != user.username:
+            user.username = form.username.data
+        if form.email.data != user.email:
+            user.email = form.email.data
+        
+        if form.image.data:
+            # Save the profile image
+            image_file = save_profile_image(form.image.data)
+            user.profile_pic = image_file
+        
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('user_bp.user_account', user_id=user.id))
+    
+    elif request.method == 'GET':
+        form.username.data = user.username
+        form.email.data = user.email
+    
+    return render_template('account_edit.html', title='Account Edit', form=form)
